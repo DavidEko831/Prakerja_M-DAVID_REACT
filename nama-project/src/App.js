67cd9273b82d61331a685543e4415996443551f3
M David <davideko618@gmail.com>
2024-06-17 21:46:47 +0700

    Initial commit

---
+++ nama-project/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/about';
+import './App.css';
+
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
